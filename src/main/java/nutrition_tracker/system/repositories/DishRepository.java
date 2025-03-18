f00bd5bf5e9eb4a813cfbaf70ad06010fd206eed
plaffyyy <k.zimin@innopolis.university>
2025-03-18 23:10:26 +0300

    feat: create logic and get for dish

---
+++ src/main/java/nutrition_tracker/system/repositories/DishRepository.java
@@ -0,0 +1,11 @@
+package nutrition_tracker.system.repositories;
+
+import nutrition_tracker.system.entities.dish.Dish;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DishRepository extends JpaRepository<Dish, Long> {
+}
---
+++ src/main/java/nutrition_tracker/system/services/DishService.java
@@ -0,0 +1,34 @@
+package nutrition_tracker.system.services;
+
+import lombok.RequiredArgsConstructor;
+import nutrition_tracker.system.dto.dish.CreateDishRequest;
+import nutrition_tracker.system.entities.dish.Dish;
+import nutrition_tracker.system.repositories.DishRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class DishService {
+
+    private final DishRepository dishRepository;
+
+    public void create(CreateDishRequest request) {
+        dishRepository.save(new Dish(
+            request.name(),
+            request.calories(),
+            request.proteins(),
+            request.fats(),
+            request.carbs()
+        ));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Dish> getAll() {
+        return dishRepository.findAll();
+    }
+
+}
