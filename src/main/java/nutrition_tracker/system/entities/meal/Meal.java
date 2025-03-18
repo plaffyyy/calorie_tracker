3ac45bfc67bac54353a05d8bbb78c6b8fd5e80d8
plaffyyy <k.zimin@innopolis.university>
2025-03-18 22:44:01 +0300

    fix: fix entities according to rules

--- src/main/java/nutrition_tracker/system/entities/meal/Meal.java
+++ src/main/java/nutrition_tracker/system/entities/meal/Meal.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import nutrition_tracker.system.entities.dish.Dish;
 import nutrition_tracker.system.entities.user.User;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -14,6 +15,9 @@ public final class Meal {
 
     @Id
     private Long id;
+
+    @Column(name = "meal_date", nullable = false)
+    private LocalDateTime mealDate;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
--- src/main/java/nutrition_tracker/system/entities/user/Goal.java
+++ src/main/java/nutrition_tracker/system/entities/user/Goal.java
@@ -1,5 +1,15 @@
 package nutrition_tracker.system.entities.user;
 
+import nutrition_tracker.system.exceptions.UndefinedGoalException;
+
 public enum Goal {
-    WEIGHT_LOSS, MAINTENANCE, WEIGHT_GAIN
+    WEIGHT_LOSS, MAINTENANCE, WEIGHT_GAIN;
+    public static Goal goalFromString(String status) {
+        return switch (status.toLowerCase()) {
+            case ("похудение") -> Goal.WEIGHT_LOSS;
+            case ("поддержание") -> Goal.MAINTENANCE;
+            case ("набор массы") -> Goal.WEIGHT_GAIN;
+            default -> throw new UndefinedGoalException("Нет такой цели");
+        };
+    }
 }
