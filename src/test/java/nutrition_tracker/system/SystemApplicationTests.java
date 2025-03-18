6cf48c2a15e3db6ae515905abc7aa097fbd014e4
plaffyyy <k.zimin@innopolis.university>
2025-03-18 11:27:58 +0300

    init: initial commit

---
+++ src/test/java/nutrition_tracker/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nutrition_tracker.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
